46c42909d6d5b40945104e608a253ee833aad531
Akash Salmuthe <68693147+AkashSalmuthe@users.noreply.github.com>
2022-04-04 13:34:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#import os
+import os
 import cv2
 import numpy as np
 from keras.preprocessing import image
@@ -10,7 +10,7 @@ from keras.preprocessing.image import load_img, img_to_array
 from keras.models import  load_model
 import numpy as np
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, WebRtcMode
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
 
 
 # Loading pre-trained parameters for the cascade classifier
@@ -26,29 +26,29 @@ RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.goog
 
 class Faceemotion(VideoTransformerBase):
     def transform(self, frame):
+        label=[]
         img = frame.to_ndarray(format="bgr24")
+        face_detect = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
+        emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
+        
+            
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_detect.detectMultiScale(gray, 1.3,1)
+        
 
-        #image gray
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_haar_cascade.detectMultiScale(
-            image=img_gray, scaleFactor=1.3, minNeighbors=5)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img=img, pt1=(x, y), pt2=(
-                x + w, y + h), color=(255, 0, 0), thickness=2)
-            roi_gray = img_gray[y:y + h, x:x + w]
-            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
-            if np.sum([roi_gray]) != 0:
-                roi = roi_gray.astype('float') / 255.0
-                roi = img_to_array(roi)
-                roi = np.expand_dims(roi, axis=0)
-                prediction = model.predict(roi)[0]
-                maxindex = int(np.argmax(prediction))
-                finalout = emotion_labels[maxindex]
-                output = str(finalout)
-            label_position = (x, y)
-            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-        return img
+        for (x,y,w,h) in faces:
+            a=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+            roi_gray = gray[y:y+h,x:x+w]
+            roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)  ##Face Cropping for prediction
+            roi = roi_gray.astype('float')/255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi,axis=0) ## reshaping the cropped face image for prediction
+            prediction = model.predict(roi)[0]   #Prediction
+            label=emotion_labels[prediction.argmax()]
+            label_position = (x,y)
+            b=cv2.putText(a,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+               
+        return b
 
 
 def main():
@@ -144,4 +144,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
